8ac14263c02d1779e4fbd03d669b2d8da231501b
Braden Everson <bradeneverson@gmail.com>
2024-12-03 15:49:28 -0600

    Add progressbar

--- examples/crawler.rs
+++ examples/crawler.rs
@@ -1,3 +1,4 @@
+use indicatif::ProgressBar;
 use page_rank_from_scratch::crawler::WebCrawler;
 
 /// How many sites to scrape for our fake internet
@@ -9,8 +10,11 @@ async fn main() {
     let mut crawler = WebCrawler::default();
     crawler.enqueue("https://www.wikipedia.org/");
 
+    let pb = ProgressBar::new(SITES_TO_SCRAPE as u64);
+
     for _ in 0..SITES_TO_SCRAPE {
         let _ = crawler.crawl().await;
+        pb.inc(1);
     }
 
     crawler.save("100_000_wiki_entries.json");
